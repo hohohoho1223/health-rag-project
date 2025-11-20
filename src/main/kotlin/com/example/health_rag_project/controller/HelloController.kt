25ae99d21660d0877e940ca3549c9ab4cb3d8011
hohohoho1223 <dnjsgh1820@naver.com>
2025-11-20 16:04:58 +0900

    feat(controller): /hello 엔드포인트 서버테스트 기능

---
+++ src/main/kotlin/com/example/health_rag_project/controller/HelloController.kt
@@ -0,0 +1,11 @@
+package com.example.health_rag_project.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String = "안녕하세요! 서버 정상 동작 확인 되었습니다!"
+}
