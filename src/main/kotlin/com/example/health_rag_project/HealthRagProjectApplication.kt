f54cfa24d566bdf678a277bf1edc76297716852b
hohohoho1223 <dnjsgh1820@naver.com>
2025-11-20 16:42:13 +0900

    chore(app): add spring boot main application class

---
+++ src/main/kotlin/com/example/health_rag_project/HealthRagProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.health_rag_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HealthRagProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<HealthRagProjectApplication>(*args)
+}
