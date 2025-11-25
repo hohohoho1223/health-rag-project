2566ff61e75bfa787f00ad7daacb62aafd3c9b87
hohohoho1223 <dnjsgh1820@naver.com>
2025-11-25 13:42:05 +0900

    feat(config): CORS 객체 추가

---
+++ src/main/kotlin/com/example/springboot_ai/config/CorsConfig.kt
@@ -0,0 +1,17 @@
+package com.example.springboot_ai.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
+            .allowedMethods("*") // 모든 HTTP 메서드 허용
+            .allowedHeaders("*") // 모든 헤더 허용
+            .allowCredentials(false) // 프론트에서 쿠키 안 쓰는 경우 false
+    }
+}
